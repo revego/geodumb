8608e6bb5eb479a7e914d744aef9b18e551bb540
lefthand <revego@code4you.cloud>
2026-03-15 11:14:16 +0100

    Change in Manifest version for GPC publication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.code4you.geodumb"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.3"
+        versionCode = 5
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // ⬇️ Questo ora funzionerà
